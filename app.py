21f2db9aec6a4fdf29d98531ada129d65468aec1
Leonid <pld02@bk.ru>
2023-05-25 16:59:39 +0300

    Fix bags

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def custom_openapi():
 
 
 app.openapi = custom_openapi
-print(type(app.openapi()))
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=4446)
--- markups.py
+++ markups.py
@@ -8,8 +8,8 @@ btn_del_photo = KeyboardButton("Удалить фото ✋🏻")
 btn_edit = KeyboardButton("Редактировать инф-ию о человеке 📝")
 btn_show_names = KeyboardButton("Вывести внесенных людей 👀")
 btn_show_person = KeyboardButton("Вывести человека с фото 👁️")
-btn_start_recognition = KeyboardButton("Начать распознование 🔍")
-btn_end_recognition = KeyboardButton("Закончить распознование 🚫")
+btn_start_recognition = KeyboardButton("Начать распознавание 🔍")
+btn_end_recognition = KeyboardButton("Закончить распознавание 🚫")
 
 main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 main_menu.add(btn_add_photo, btn_del_photo)
@@ -20,15 +20,15 @@ main_menu.add(btn_start_recognition)
 main_menu.add(btn_end_recognition)
 
 inline_btn_cancel = InlineKeyboardButton(text="Отмена",
-                     callback_data='cancel')
+                                         callback_data='cancel')
 
 inline_menu = InlineKeyboardMarkup()
 inline_menu.add(inline_btn_cancel)
 
 inline_btn_name = InlineKeyboardButton(text="Изменить имя",
-                     callback_data='name')
+                                       callback_data='name')
 inline_btn_photo = InlineKeyboardButton(text="Поменять фото",
-                     callback_data='photo')
+                                        callback_data='photo')
 
 inline_edit_menu = InlineKeyboardMarkup()
 inline_edit_menu.add(inline_btn_name, inline_btn_photo)
